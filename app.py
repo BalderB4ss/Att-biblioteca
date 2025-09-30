ad3b660fe78182709ff9a86e43c25abd8eda8093
Balder <balder.ansgar@gmail.com>
2025-09-30 16:39:42 -0300

    Melhorias no streamlit

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ import pandas as pd
 import sqlite3
 
 st.title("Biblioteca Online")
-
+st.logo("logo.gif")
+st.image("imagem1.png", width=400)
 aba1,aba2,aba3,aba4 = st.tabs(["Cadastrar Livro","Listar Livros","Remover Livro","Mudar Disponibilidade"])
 with aba1:
+    st.markdown("Cadastrar um novo livro📚")
     fnc.cadastrar_livro()
 with aba2:
+    st.markdown("Todos os livros registrados🤓")
     fnc.listar_livros()
 with aba3:
+    st.markdown("Remover cadastro de livro❗")
     fnc.remover_livro()
 with aba4:
+    st.markdown("Mudar disponibilidade✅")
     fnc.disponibilidade()
